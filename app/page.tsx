ecd630d0c7c1f35717ddf09c012328446ff0a0c5
bossnare <razafimangagervaischristo@gmail.com>
2026-08-22 23:17:17 +0300

    change styling home page

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             </span>{' '}
             Universe.
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400 animate-pulse">
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-500 animate-pulse">
             Building the Future...
           </p>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
