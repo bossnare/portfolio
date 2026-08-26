7dbe2a4da1bb798815b75316c6d2aeb3f755273a
bossnare <razafimangagervaischristo@gmail.com>
2026-08-26 22:03:33 +0300

    add cv

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/next';
-import { GoogleAnalytics } from '@next/third-parties/google';
+// import { GoogleAnalytics } from '@next/third-parties/google';
 import './globals.css';
 
 const geistSans = Geist({
@@ -33,7 +33,7 @@ export default function RootLayout({
         {children}
 
         <Analytics />
-        <GoogleAnalytics />
+        {/* <GoogleAnalytics /> */}
       </body>
     </html>
   );
