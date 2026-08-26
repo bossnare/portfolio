b885d6f5720ef1d44bbf5e7022b17907d36701a7
bossnare <razafimangagervaischristo@gmail.com>
2026-08-26 19:04:36 +0300

    add config analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import Analytics from '@vercel/analytics'
+import { Analytics } from '@vercel/analytics/next';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import './globals.css';
 
 const geistSans = Geist({
@@ -28,9 +29,11 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}
+      <body className="min-h-full flex flex-col">
+        {children}
 
         <Analytics />
+        <GoogleAnalytics />
       </body>
     </html>
   );
