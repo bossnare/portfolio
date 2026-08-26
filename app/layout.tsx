27ac12eec728b219552c1680051212f1db255fad
bossnare <razafimangagervaischristo@gmail.com>
2026-08-26 11:51:11 +0300

    corige analytics import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import {Analytics} from '@vercel/analytics'
+import Analytics from '@vercel/analytics'
 import './globals.css';
 
 const geistSans = Geist({
