57f06a3c8a13a05856e9d4633a75cc7f154763db
bossnare <razafimangagervaischristo@gmail.com>
2026-09-13 21:30:24 +0300

    add subtle dot grid in profile picture on Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { stacks } from '@/src/data/stacks';
-import { MoveRight, Send, Terminal } from 'lucide-react';
+import { MoveRight, Plus, Send, Terminal } from 'lucide-react';
 import { motion, useInView } from 'motion/react';
 import Image from 'next/image';
 import { useRef } from 'react';
@@ -91,7 +91,7 @@ export function Hero() {
           </ul>
         </div>
       </div>
-      <div className="relative overflow-hidden md:w-1/2 bg-[linear-gradient(to_right,rgba(148,163,184,0.35)_1px,transparent_1px),linear-gradient(to_bottom,rgba(148,163,184,0.35)_1px,transparent_1px)] bg-size-[40px_40px]">
+      <div className="relative overflow-hidden md:w-1/2">
         <motion.div
           ref={ref}
           initial={{ opacity: 0, y: 50 }}
@@ -99,7 +99,7 @@ export function Hero() {
           transition={{ duration: 0.6 }}
         >
           <Image
-            className="relative object-contain h-auto mx-auto w-70 lg:w-105 sm:w-85 xl:w-120 z-12"
+            className="relative object-contain h-auto mx-auto w-80 lg:w-105 sm:w-85 xl:w-120 z-12"
             src="/images/profile-1.png"
             alt="Christo Razafimanga"
             width={800}
@@ -107,7 +107,8 @@ export function Hero() {
             priority
           />
         </motion.div>
-        <div className="absolute inset-0 z-10"></div>
+        {/* dot grid */}
+        <div className="absolute inset-0 z-10 bg-[radial-gradient(circle,rgba(148,163,184,0.6)_1px,transparent_1px)] bg-size-[24px_24px] opacity-50 mask-[radial-gradient(circle_at_center,black_35%,transparent_100%)] [-webkit-mask-image:radial-gradient(circle_at_center,black_35%,transparent_100%)]"></div>
         <Image
           className="absolute top-0 object-contain h-auto -right-10 w-35 lg:w-45 sm:w-40 xl:w-50 z-11"
           src="/icon/graphic-1.svg"
@@ -124,16 +125,20 @@ export function Hero() {
           height={600}
           priority
         />
-        {/* <div className="absolute inset-0 z-9 bg-[radial-gradient(ellipse_at_center,transparent_20%,#fff_70%)]"></div> */}
+        <div className="absolute z-20 rounded-full border-3 size-4 md:size-5 border-primary top-10 left-8 md:top-20 md:left-30"></div>
+        <div className="absolute z-20 bg-[#F0785E] rounded-full size-2 bottom-20 right-4 md:size-4 md:bottom-26 md:right-16"></div>
+        <div className="absolute z-20 rounded-full text-[#55D5D0] bottom-50 right-14 md:bottom-80 md:right-40">
+          <Plus className="size-5" />
+        </div>
       </div>
 
       {/* availability */}
       <div className="absolute flex items-center p-2 border rounded-lg bottom-4 right-8 bg-background/90 z-16 border-zinc-300 dark:border-white/6">
         <span className="rounded-full size-2.5 bg-primary ml-1"></span>
-        <span className="ml-2 text-muted-foreground">
+        <span className="ml-2 text-sm md:text-base text-muted-foreground">
           Available for new opportunities
         </span>
-        <span className="p-1.5 border rounded-md border-zinc-300 dark:border-white/6">
+        <span className="p-1 border rounded-md border-zinc-300 dark:border-white/6">
           <Terminal className="text-primary size-5" />
         </span>
       </div>
