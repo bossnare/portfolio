4c2cb0ce9427b424e014772c45840a350ed5414a
bossnare <razafimangagervaischristo@gmail.com>
2026-09-15 21:33:21 +0300

    adjust element size to smaller and realistic

--- src/components/hero/HeroVisual.tsx
+++ src/components/hero/HeroVisual.tsx
@@ -43,7 +43,7 @@ export function HeroVisual() {
         height={600}
         priority
       />
-      <div className="absolute z-20 rounded-full border-3 size-2 md:size-3 border-primary top-10 left-8 md:top-20 md:left-30"></div>
+      <div className="absolute z-20 rounded-full border-3 size-3 md:size-4 border-primary top-10 left-8 md:top-20 md:left-30"></div>
       <div className="absolute z-20 bg-[#F0785E] rounded-full size-2 bottom-20 right-4 md:size-3 md:bottom-26 md:right-16"></div>
       <div className="absolute z-20 rounded-full text-[#55D5D0] bottom-50 right-14 md:bottom-80 md:right-40">
         <Plus className="size-5" />
