223012efbe891d8f5914936a1d53ba495215e383
bossnare <razafimangagervaischristo@gmail.com>
2026-09-19 08:39:53 +0300

    fix: sitempa.ts objects

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,7 +7,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
         lastModified: new Date(),
         changeFrequency: "monthly",
         priority: 1,
-        sitemap: "https://christorazafimanga.is-a.dev"
     }
     ]
 
