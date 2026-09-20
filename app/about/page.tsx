8da5039a888aeb214e040ebb5817ba107a798d96
bossnare <razafimangagervaischristo@gmail.com>
2026-09-20 20:19:22 +0300

    refactor Page component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from 'next';
-import {PageHeader} from '@/src/components/PageHeader'
+import {Page, PageHeader} from '@/src/components/Page'
 
 export const metadata: Metadata = {
   title: 'About',
 };
 
 export default function AboutPage() {
-  return (<div className="w-full max-w-6xl px-4 mx-auto space-y-6 md:pt-12 md:p-0">
+  return (<Page id="about-page">
     <PageHeader title="About" description="A little bit about me and what I do." />
-  </div>);
+  </Page>);
 }
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,3 +1,11 @@
+import {cn} from '@/src/lib/utils'
+
+type PageProps = React.ComponentProps<'div'>;
+
+export const Page = ({className, children, ...props}: PageProps) => {
+    return <div className={cn("w-full max-w-6xl px-4 mx-auto space-y-6 md:pt-14 md:px-0", className)} {...props}>{children}</div>
+}
+
 export function PageHeader({title="Title", description='This is the description'}: {title?: string; description?: string}) {
     return(
         <div className="space-y-4">
