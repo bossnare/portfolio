dee1738da9d56695fce30822279f92930f5fb0ef
bossnare <razafimangagervaischristo@gmail.com>
2026-09-22 12:15:08 +0300

    fix: dynamic import typo, usage

--- src/components/location-map.tsx
+++ src/components/location-map.tsx
@@ -1,19 +1,17 @@
-"use client"
+'use client';
 
-import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic';
 
-const LocationMapClient = dynamic((
-    () => import('@/src/components/location-map-client'),
-    {
-        ssr: false,
-        loading: (<div className="h-100 w-full bg-zinc-300 dark:bg-zinc-800 animate-pulse rounded-3xl" />)
-  }
+const LocationMapClient = dynamic(
+  () => import('./location-map-client').then((module) => module.LocationMapClient),
+  {
+    ssr: false,
+    loading: () => (
+      <div className="h-100 w-full animate-pulse rounded-3xl bg-zinc-300 dark:bg-zinc-800" />
+    ),
+  },
 );
-    }
-))
 
 export function LocationMap() {
-    return(
-        <LocationMapClient />
-    )
+  return <LocationMapClient />;
 }
